1ec200106e8388194a8b00d73a60828e2c0784a7
Гыук <artur_lychik@mail.ru>
2025-06-19 21:10:32 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello, World!");
+        int[] weights = new int[12];
+        weights[0] = 90;
+        int jenvaryWeights = weights[0];
+        System.out.println(jenvaryWeights);
     }
 }
