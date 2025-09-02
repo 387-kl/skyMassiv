de34dca81383d640ca658df5ea414b480890f7a3
Гыук <artur_lychik@mail.ru>
2025-09-02 16:19:17 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
+import javax.naming.NameAlreadyBoundException;
+import java.io.PrintStream;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int[] weights = new int[12];
-        weights[0] = 90;
-        int jenvaryWeights = weights[0];
-        System.out.println(jenvaryWeights);
+        /// Tesk1
+        System.out.println("Tesk1");
+        double[] weights = {90,91,93,92,85,87,84,83,0,0,0,0};
+        weights [0] = 1.57;
+        weights [1] = 7.654;
+        weights [2] = 9.986;
+        System.out.println(Arrays.toString(weights));
+    ///Tesk2
+        System.out.println("Tesk2");
+        double[] arr = new double[3];
+        arr [0] = 1.57;
+        arr [1] = 7.654;
+        arr [2] = 9.986;
+        System.out.println(Arrays.toString(arr));
+    ///Teck3
+        System.out.println("Tesk3");
+
     }
-}
+    }
