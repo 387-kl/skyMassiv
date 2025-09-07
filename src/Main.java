c24d48dbcc69a5dfad1b8a5e744a5016333a6ba0
Гыук <artur_lychik@mail.ru>
2025-09-07 09:59:49 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,36 @@ public class Main {
         System.out.println("Hello, World!");
         /// Tesk1
         System.out.println("Tesk1");
-        double[] weights = {90,91,93,92,85,87,84,83,0,0,0,0};
-        weights [0] = 1.57;
-        weights [1] = 7.654;
-        weights [2] = 9.986;
+        double[] weights = {90, 91, 93, 92, 85, 87, 84, 83, 0, 0, 0, 0};
+        weights[0] = 1.57;
+        weights[1] = 7.654;
+        weights[2] = 9.986;
         System.out.println(Arrays.toString(weights));
-    ///Tesk2
+        ///Tesk2
         System.out.println("Tesk2");
-        double[] arr = new double[3];
-        arr [0] = 1.57;
-        arr [1] = 7.654;
-        arr [2] = 9.986;
+        double[] arr = {1, 2, 3};
+        arr[0] = 1.57;
+        arr[1] = 7.654;
+        arr[2] = 9.986;
         System.out.println(Arrays.toString(arr));
-    ///Teck3
+        ///Teck3
         System.out.println("Tesk3");
-
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.println(arr[i]);
+            if (i > 0) {
+                System.out.println(", ");
+            }
+            System.out.println("");
+        }
+        /// Tesk4
+        System.out.println("Tesk4");
+        for (int index = 0; index < arr.length; index++) {
+            arr[index] = + 1;
+            if (index == arr.length - 1) {
+                System.out.println(arr[index]);
+                break;
+            }
+            System.out.println("");
+        }
     }
-    }
+}
