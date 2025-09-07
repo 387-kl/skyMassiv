50ed15a6c4467e9f3a3a1f31d6d7bace18da2087
Гыук <artur_lychik@mail.ru>
2025-09-07 10:07:46 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -36,7 +36,7 @@ public class Main {
                 System.out.println(arr[index]);
                 break;
             }
-            System.out.println("+");
+            System.out.println("");
         }
     }
 }
